ee57d474ba6697f2c75807134dbb5b10ed1a61e9
Pilgrimage <nanobot149@gmail.com>
2017-09-22 21:44:05 +0300

    Lab is done.

---
+++ Ch01_IntroToNETCoreAndEFCore/p01_StudentSystem/StartUp.cs
@@ -0,0 +1,9 @@
+﻿namespace p01_StudentSystem
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ Ch01_IntroToNETCoreAndEFCore/p02_SocialNetwork/StartUp.cs
@@ -0,0 +1,9 @@
+﻿namespace p02_SocialNetwork
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
